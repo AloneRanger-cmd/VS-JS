82d81c6cda98b2acaf5228ff47ffd83c3f637009
Burak <burakatak2001@gmail.com>
2026-03-10 14:10:06 +0000

    modified:   PFDM/Assignment 1 PFDM/sketch.js 	modified:   PFDM/Worksheet 4 PFDM/sketch.js

--- PFDM/Assignment 1 PFDM/sketch.js
+++ PFDM/Assignment 1 PFDM/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(220);
+    background(120);
+    noLoop();
+    // Draw 20 random circles and squares
+    for (let i = 0; i < 20; i++) {
+        let x = random(0, windowWidth);
+        let y = random(0, windowHeight);
+        let size = random(10, 150);
+        fill(random(255), random(255), random(255));
+        ellipse(x, y, size, size);
+        fill(random(255), random(255), random(255));
+        square(x, y, size);
+    }
 }
--- PFDM/Worksheet 4 PFDM/sketch.js
+++ PFDM/Worksheet 4 PFDM/sketch.js
@@ -7,14 +7,12 @@ let wh = 0;
 function draw() {
     background(255);
     noLoop();
-    for (let ix = 0; ix < 3; ix++) {
-        let dotX = 100 + ix * 100;
-        for (let iy = 0; iy < 3; iy++) {
-            let dotY = 100 + iy * 100;
+    for (let dotX = 100; dotX < 400; dotX += 100) {
+        for (let dotY = 100; dotY < 400; dotY += 100) {
             for (let i = 0; i < 31; i++) {
-                wh = i < 30 ? wh + 2.9 : 12;
+                let wh = i < 30 ? (i + 1) * 2.9 : 0;
                 stroke(30)
-                strokeWeight(1.5);
+                strokeWeight(1.3);
                 strokeCap(ROUND);
                 noFill();            
                 arc(dotX, dotY, wh, wh, random(0,360), random(0,360), OPEN);
