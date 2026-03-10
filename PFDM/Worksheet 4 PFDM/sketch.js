cdd2260d2e29a6c66b20d0b3db8bbb454bbc3c60
Burak <burakatak2001@gmail.com>
2026-03-10 14:23:29 +0000

    modified:   PFDM/Worksheet 4 PFDM/sketch.js

--- PFDM/Worksheet 4 PFDM/sketch.js
+++ PFDM/Worksheet 4 PFDM/sketch.js
@@ -7,15 +7,15 @@ let wh = 0;
 function draw() {
     background(255);
     noLoop();
-    for (let dotX = 100; dotX < 400; dotX += 100) {
-        for (let dotY = 100; dotY < 400; dotY += 100) {
+    for (let X = 100; X < 400; X += 100) {
+        for (let Y = 100; Y < 400; Y += 100) {
             for (let i = 0; i < 31; i++) {
                 let wh = i < 30 ? (i + 1) * 2.9 : 0;
                 stroke(30)
                 strokeWeight(1.3);
                 strokeCap(ROUND);
                 noFill();            
-                arc(dotX, dotY, wh, wh, random(0,360), random(0,360), OPEN);
+                arc(X, Y, wh, wh, random(0,360), random(0,360), OPEN);
             }
         }
     }
