8c7937567751b06d955f206474bb66fe098c5c7b
Burak <burakatak2001@gmail.com>
2026-03-14 16:35:48 +0000

    Some File Changes 	modified:   PFDM/WorkSheet 6 PFDM/sketch.js 	modified:   PFDM/Worksheet 5 PFDM/README.md

--- PFDM/WorkSheet 6 PFDM/sketch.js
+++ PFDM/WorkSheet 6 PFDM/sketch.js
@@ -1,22 +1,25 @@
 function setup() {
     createCanvas(600, 600);
     rectMode(CENTER);
+    angleMode(DEGREES);
 }
-
+let r = 90;
 function draw() {
     background(0);
     noLoop();
     noStroke();
     fill(255, 0, 0);
-    let r = 90;
+    
     for (let x = 50; x < width; x += 100) {
         for (let y = 50; y < height; y += 100) {
             let i = random(100);
-            console.log(i);
-            if (i > 25) {
+            console.log(i);5
+            if (i > 33 && i < 66) {
                 ellipse(x, y, r, r);
+            } else if (i > 66)  {
+                arc(x, y, r, r, random(0,360), random(0,360), PIE);
             } else {
-                rect(x,y , r, r);
+                rect(x, y, r, r);
             }
         }
     }
