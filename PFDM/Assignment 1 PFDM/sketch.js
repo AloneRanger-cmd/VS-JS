2671688e6a1829b62e596a31e7ef0d19dce551f3
Burak <burakatak2001@gmail.com>
2026-03-27 05:14:02 +0000

    PFDM Assignment 1 PFDM and For Loops Dersi 	modified:   PFDM/Assignment 1 PFDM/sketch.js 	modified:   "Personal/T\303\274rk\303\247e Ders Anlatam\304\261/For Loops Dersi/sketch.js"

--- PFDM/Assignment 1 PFDM/sketch.js
+++ PFDM/Assignment 1 PFDM/sketch.js
@@ -1,18 +1,123 @@
+// Programming for Digital Media
+// Assignment 1 /
+// Burak Atak – 3199405 – burakatak2001@gmail.com
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 800);
+    // For Degrees instead of Radians.
+    angleMode(DEGREES);
+    // For centerıng the squares.
+    rectMode(CENTER);
 }
 
 function draw() {
-    background(120);
+    background(0);
     noLoop();
-    // Draw 20 random circles and squares
-    for (let i = 0; i < 20; i++) {
-        let x = random(0, windowWidth);
-        let y = random(0, windowHeight);
-        let size = random(10, 150);
-        fill(random(255), random(255), random(255));
-        ellipse(x, y, size, size);
-        fill(random(255), random(255), random(255));
-        square(x, y, size);
+    
+    // The random function generates a random number between 0 and 1. If the generated number is greater than 0.5, the first block of code is executed, else second block of code is executed.
+    let dice = random(1);
+    // First Block
+    if (dice > 0.5) {
+        // Background triangles color is opposite compered to the second block.
+        for (let x1 = 0; x1 < width+1; x1 += 50) {
+            for (let y1 = 0; y1 < height+1; y1 += 50) {
+                noFill();
+                stroke(30);
+                strokeWeight(1);
+                // Creating two distinct rows of triangles by checking the y-coordinate. If the y-coordinate matches any of the specified values (50, 150, 250, etc.), it draws a triangle with a specific stroke color.
+                if (y1 == 50 || y1 == 150 || y1 == 250 || y1 == 350 || y1 == 450 || y1 == 550 || y1 == 650 || y1 == 750) {   
+                    triangle(x1+25, y1+50, x1, y1, x1+50, y1)
+                } else {
+                    stroke(random(255), random(110), random(20));
+                    triangle(x1+25, y1+50, x1, y1, x1+50, y1)
+                }
+            }
+        }
+        // The nested loops iterate through the grid points at intervals of 200 pixels, starting from 200 and ending before the width and height of the canvas.
+        for (let x = 200; x < width; x += 200) {
+            for (let y = 200; y < height; y += 200) {
+                // The innermost loop runs 80 times for each grid point, creating a series of arcs with varying sizes and colors. The variable 'wh' determines the width and height of the arcs, 
+                // increasing with each iteration until it resets to 0 on the last iteration.
+                for (let i = 0; i < 80; i++) {
+                    let wh;
+                    if (i < 79) {
+                        wh = (i + 1) *  2.9;
+                    } else {
+                        wh = 0;
+                    }
+                    stroke(random(0), random(200), random(150));
+                    strokeWeight(random(1, 5));
+                    strokeCap(ROUND);
+                    noFill();
+                    // The if statement checks if the current coordinates (x, y) correspond to the corners of the grid (200, 200), (600, 200), (200, 600), and (600, 600). 
+                    // If they do, it draws an arc at those positions. This simplyfies the code by eliminating the need for separate loops or conditions for the corners,
+                    // allowing the same drawing logic to be applied to all points while only executing the arc drawing for the specified corner positions.
+                    if (x == 200 && y == 200 || x == 600 && y == 200 || x == 200 && y == 600 || x == 600 && y == 600) {          
+                        arc(x, y, wh, wh, random(0,360), random(0,360), OPEN);
+                    } else {
+                    }
+                }
+                // Square Drawing
+                strokeWeight(random(5, 10));
+                square(width/2, height/2, random(300, 400));
+                square(width/2, height/2, random(600, 800));
+            }
+        }
+    }
+    // Second Block
+    else {
+        // Background triangles color is reversed compered to the first block.
+        for (let x1 = 0; x1 < width+1; x1 += 50) {
+            for (let y1 = 0; y1 < height+1; y1 += 50) {
+                noFill();
+                stroke(30);
+                strokeWeight(1);
+                // Creating two distinct rows of triangles by checking the y-coordinate. If the y-coordinate matches any of the specified values (50, 150, 250, etc.), it draws a triangle with a specific stroke color.
+                if (y1 == 50 || y1 == 150 || y1 == 250 || y1 == 350 || y1 == 450 || y1 == 550 || y1 == 650 || y1 == 750) {   
+                    triangle(x1+25, y1+50, x1, y1, x1+50, y1)
+                } else {
+                    stroke(random(0), random(200), random(150));
+                    triangle(x1+25, y1+50, x1, y1, x1+50, y1)
+                }
+            }
+        }
+        // The nested loops iterate through the grid points at intervals of 200 pixels, starting from 200 and ending before the width and height of the canvas.
+        for (let x = 200; x < width; x += 200) {
+            for (let y = 200; y < height; y += 200) {
+                // The innermost loop runs 80 times for each grid point, creating a series of arcs with varying sizes and colors. The variable 'wh' determines the width and height of the arcs, 
+                // increasing with each iteration until it resets to 0 on the last iteration.
+                for (let i = 0; i < 80; i++) {
+                    let wh;
+                    if (i < 79) {
+                        wh = (i + 1) *  2.9;
+                    } else {
+                        wh = 0;
+                    }
+                    stroke(random(255), random(110), random(20));
+                    strokeWeight(random(1, 5));
+                    strokeCap(ROUND);
+                    noFill();
+                    // The if statement checks if the current coordinates (x, y) correspond to the corners of the grid (400, 200), (200, 400), (600, 400), and (400, 600). 
+                    // If they do, it draws an arc at those positions. This simplyfies the code by eliminating the need for separate loops or conditions for the corners,
+                    // allowing the same drawing logic to be applied to all points while only executing the arc drawing for the specified corner positions.      
+                    if (x == 400 && y == 200 || x == 200 && y == 400 || x == 600 && y == 400 || x == 400 && y == 600) {          
+                        arc(x, y, wh, wh, random(0,360), random(0,360), OPEN);
+                    } else {
+                    }
+                    
+                }
+                // from push to pop, the code applies transformations to the drawing context, allowing for the creation of rotated squares at the center of the canvas.
+                // this six lines of code are responsible for translating the origin to a new position, rotating the drawing context by 45 degrees,
+                // setting a random stroke weight, and drawing a square with random size at the center of the canvas.
+                // These six lines of code is inpired by 8: Translate and Rotate in p5.js: How to Code Generative Art by Steve"s Makerspace on YouTube.
+                // Link for the video: https://www.youtube.com/watch?v=H9PeFYqi-V0
+                push();
+                translate(400, -165);
+                rotate(45);
+                strokeWeight(random(5, 10));
+                square(width/2, height/2, random(200, 400));
+                square(width/2, height/2, random(600, 800));
+                pop();
+            }
+        }
     }
 }
--- Personal/Türkçe Ders Anlatamı/For Loops Dersi/sketch.js
+++ Personal/Türkçe Ders Anlatamı/For Loops Dersi/sketch.js
@@ -1,6 +1,5 @@
 //For Loops Dersi Anlatımı
 
-
 function setup() {
     createCanvas(600, 600);
 }
@@ -9,6 +8,7 @@ function draw() {
     background(220, 100, 40);
     stroke(255);
     strokeWeight(8);
+    //Kodun surekli calisip rasgele degerler uretmesini engeller
     noLoop();
     // Çizgi sonlarını Yuvarlak yapar
     strokeCap(ROUND);
@@ -16,14 +16,15 @@ function draw() {
     let x1;
     let x2;
 
-    //Loop Başlangıç
-for (let y = 50; y < height; y += 50) {
-    //Random x1 ve x2 değerleri oluşturma
-    //x1 değeri 50 ile canvasın yarısı arasında olacak şekilde oluşturulur
-    x1 = random(50, width / 2);
-    //x2 değeri canvasın yarısı ile canvasın sonu arasında olacak şekilde oluşturulur ama x1 değerine göre ayarlanır
-    x2 = width / 2 + (width / 2 - x1);
-    //Çizgi çizme
-    line(x1, y, x2, y);
+    //Loop Başlangıç Bu Loop 50 ile canvasın sonu arasında 50'şer artarak y değerleri oluşturur.
+    //y degerleri cizgiler arasindaki mesafeyi belirler
+    for (let y = 50; y < height; y += 50) {
+        //Random x1 ve x2 değerleri oluşturma
+        //x1 değeri 50 ile canvasın yarısı arasında olacak şekilde oluşturulur
+        x1 = random(50, width / 2);
+        //x2 değeri canvasın yarısı ile canvasın sonu arasında olacak şekilde oluşturulur ama x1 değerine göre ayarlanır
+        x2 = width / 2 + (width / 2 - x1);
+        //Çizgi çizme
+        line(x1, y, x2, y);
     }
 }
